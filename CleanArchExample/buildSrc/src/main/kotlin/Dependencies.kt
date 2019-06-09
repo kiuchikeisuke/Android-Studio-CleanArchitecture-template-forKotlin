bc71c384cdc57d577925a69eeef41359c0ae27e1
kiuchikeisuke <minismahn@yahoo.co.jp>
2019-06-09 15:55:32 +0900

    add deps

--- CleanArchExample/buildSrc/src/main/kotlin/Dependencies.kt
+++ CleanArchExample/buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,6 +2,28 @@ object Versions {
     const val compileSdk = 28
     const val minSdk = 21
     const val targetSdk = 28
+    const val androidGradle = "3.4.1"
 
     const val kotlin = "1.3.31"
+
+    const val appCompat = "1.0.2"
+    const val constraintLayout = "1.1.3"
+
+}
+
+@Suppress("Reformat")
+object Deps {
+    object GradlePlugin {
+        const val android = "com.android.tools.build:gradle:${Versions.androidGradle}"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    }
+
+    object Kotlin {
+        const val stdlibJdk = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    }
+
+    object AndroidX {
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    }
 }
